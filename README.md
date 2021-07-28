9e2c9108cf1198ad158064bec3ca90c4037f355b
wgoins6 <wgoins6@gatech.edu>
2021-07-28 01:12:00 -0600

    Initial implementation of the Chaos Game

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # ChaosGame
+
+Fractal generator inspired by the [Numberphile video](https://www.youtube.com/watch?v=kbKtFN71Lfs&t=407s&pp=sAQA):
---
+++ source/origin.py
@@ -0,0 +1,54 @@
+import pygame
+from pygame import gfxdraw
+from pygame import Color
+import random
+
+def main():
+    pixel_positions = []
+    latest_pixel_pos = None
+    start_simulation = False
+    original_length = 0
+
+    pygame.init()
+    screen = pygame.display.set_mode((800, 800))
+    pygame.Surface.fill(screen, Color(0, 0, 0))
+
+    running = True
+    while running:
+        pygame.display.update()
+        for event in pygame.event.get():
+            if event.type == pygame.MOUSEBUTTONUP:
+                pos = pygame.mouse.get_pos()
+                pixel_positions.append(pos)
+                gfxdraw.pixel(screen, pos[0], pos[1], Color(255, 255, 255))
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RETURN:
+                    start_simulation = not start_simulation
+            if event.type == pygame.QUIT:
+                running = False
+
+        if start_simulation:
+            if not latest_pixel_pos:
+                original_length = len(pixel_positions)
+                rand_pos1 = pixel_positions[random.randint(0, len(pixel_positions) - 1)]
+                rand_pos2 = pixel_positions[random.randint(0, len(pixel_positions) - 1)]
+                while rand_pos1 == rand_pos2:
+                    rand_pos2 = pixel_positions[random.randint(0, len(pixel_positions))]
+                mp = get_midpoint(rand_pos1, rand_pos2)
+                gfxdraw.pixel(screen, mp[0], mp[1], Color(255, 255, 255))
+                latest_pixel_pos = mp
+            else:
+                rand_pos = pixel_positions[random.randint(0, original_length - 1)]
+                mp = get_midpoint(rand_pos, latest_pixel_pos)
+                gfxdraw.pixel(screen, mp[0], mp[1], Color(255, 255, 255))
+                latest_pixel_pos = mp
+
+
+def get_midpoint(pos1, pos2):
+    new_x = (pos1[0] + pos2[0]) // 2
+    new_y = (pos1[1] + pos2[1]) // 2
+    return (new_x, new_y)
+
+
+if __name__=="__main__":
+    main()
